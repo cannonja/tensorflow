87937cd5d8c1c0088abf6eb43ec4512342de73e2
cannonja <jack_cannon@live.com>
2016-07-26 13:30:18 -0700

    Ran CNN tutorial

--- tensorflow/examples/tutorials/mnist/my_files/MNIST_exp/multi_layer.py
+++ tensorflow/examples/tutorials/mnist/my_files/MNIST_exp/multi_layer.py
@@ -2,44 +2,140 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-## Read and unpack images
+## Read and unpack images, then launch interactive session
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-## Launch interactive session
-## Interavtive session allows us to interleave operations that build graph
-## with those that run/execute graph
-## Regular session makes you build first, then execute
 sess = tf.InteractiveSession()
 
-########## Build softmax regression single layer ####################
 
-## Start building computation graph by defining placeholders (inputs and target labels)
+
+############ Write helper functions ##################################
+
+## Weight initialization
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+## Convolution and pooling
+## Conv stride = 1, conv pad = 0
+## Max pooling = 2x2 blocks
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1], padding='SAME')
+
+
+############# Define input and target label placeholders #############
+
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-## Then define variables, generally model parameters (weights, biases)
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
 
-## Initialize variables so they can be used within the session
-sess.run(tf.initialize_all_variables())
+############## Implement first convolutional layer ####################
 
-## Add activation node
-y = tf.nn.softmax(tf.matmul(x,W) + b)
+'''
+Will consist of convolution followed by max pooling
+Convolution has 32 feature maps over 5x5 patches
+Its weight tensor will have a shape of [5, 5, 1, 32].
+The first two dimensions are the patch size, the next is the number of input channels,
+and the last is the number of output channels.
+We will also have a bias vector with a component for each output channel.
+'''
 
-## Define Loss
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+## Define weights and biases
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+'''
+To apply the layer, we first reshape x to a 4d tensor, with the second and
+third dimensions corresponding to image width and height, and the final dimension
+corresponding to the number of color channels.
+'''
+x_image = tf.reshape(x, [-1,28,28,1])
 
 
-########### Train the model ###########################################
+'''
+We then convolve x_image with the weight tensor, add the bias,
+apply the ReLU function, and finally max pool.
+'''
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-for i in range(1000):
-      batch = mnist.train.next_batch(50)
-      train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
 
-########### Evaluate ##################################################
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+################# Implement second convolutional layer #######################
+
+## Second layer has 64 featur maps for each 5x5 patch
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+
+################ Fully connected layer ######################################
+
+'''
+Now that the image size has been reduced to 7x7, we add a fully-connected
+layer with 1024 neurons to allow processing on the entire image.
+We reshape the tensor from the pooling layer into a batch of vectors,
+multiply by a weight matrix, add a bias, and apply a ReLU.
+'''
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+
+############## Dropout #####################################################
+'''
+To reduce overfitting, we will apply dropout before the readout layer.
+We create a placeholder for the probability that a neuron's output is kept
+during dropout. This allows us to turn dropout on during training, and turn it
+off during testing. TensorFlow's tf.nn.dropout op automatically handles scaling
+neuron outputs in addition to masking them, so dropout just works without
+any additional scaling
+'''
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+############## Final softmax layer (readout/output layer) ##################
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
+############## Train and evaluate ##########################################
+'''
+How well does this model do? To train and evaluate it we will use code that
+is nearly identical to that for the simple one layer SoftMax network above.
+The differences are that: we will replace the steepest gradient descent optimizer
+with the more sophisticated ADAM optimizer; we will include the additional
+parameter keep_prob in feed_dict to control the dropout rate; and we will add
+logging to every 100th iteration in the training process.
+'''
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x:batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g"%(i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
